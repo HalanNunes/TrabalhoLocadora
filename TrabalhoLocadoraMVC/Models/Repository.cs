44e40fe40a7ace49602d9f81b8a95b5d4b0e5c9c
Halan Nunes <halan.nunes@gmail.com>
2013-11-26 21:52:24 -0200

    Start Project

---
+++ TrabalhoLocadoraMVC/Models/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoLocadoraMVC.Models
+{
+    public class Repository : DbContext
+    {
+        public Repository() : base("DefaultConnection") { }
+
+        public DbSet<Cliente> Clientes { set; get; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
